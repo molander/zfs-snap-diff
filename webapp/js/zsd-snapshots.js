05dcc150f551006569070e56bb9e7cbce1d46b5c
unknown <peter.southwell1970@gmail.com>
2018-04-20 15:32:33 -0700

    Fix javascript error

--- webapp/js/zsd-snapshots.js
+++ webapp/js/zsd-snapshots.js
@@ -1,5 +1,5 @@
 angular.module('zsdSnapshots', []).
-  directive('zsdSnapshots', ['$location', '$anchorScroll', 'Backend', function($location, $anchorScroll, Backend){
+  directive('zsdSnapshots', ['$location', '$anchorScroll', 'Backend', function ($location, $anchorScroll, Backend) {
     return {
       restrict: 'E',
       templateUrl: 'template-snapshots.html',
@@ -8,64 +8,64 @@ angular.module('zsdSnapshots', []).
         title: '@',
         onSnapshotSelected: '&'
       },
-      link: function(scope, element, attrs){
-        
-        scope.snapshotSelected = function(snap){
+      link: function (scope, element, attrs) {
+
+        scope.snapshotSelected = function (snap) {
           //scope.hideSnapshots = true;
 
           scope.curSnap = snap;
-          scope.onSnapshotSelected({snap: snap});
-          
+          scope.onSnapshotSelected({ snap: snap });
+
         };
 
-          
-          // restore the file from the selected snapshot
-          scope.restoreSnapshot = function($event, snap) {
-            $event.preventDefault();
-            
 
-            var r = confirm("Press a button!");
+        // restore the file from the selected snapshot
+        scope.restoreSnapshot = function ($event, snap) {
+          $event.preventDefault();
+
+
+          var r = confirm("Press a button!");
           if (r == true) {
-            Backend.restoreSnapshot($scope.curSnap.Name).then(function(res){
+            Backend.restoreSnapshot($scope.curSnap.Name).then(function (res) {
               $rootScope.$broadcast('zsd:success', res);
               $scope.lastAction()
+            }).catch((err) => {
+              var string = JSON.stringify(err);
+              alert("Error occured: " + string);
             });
+  ;
           }
-           
-          }.catch((err) => {
-            var string = JSON.stringify(err);
-            alert("Error occured: " + string);
-          });
-  
-        
-        scope.toggleHideSnapshots = function(){
-          scope.hideSnapshots = ! scope.hideSnapshots;
+
+        }
+
+        scope.toggleHideSnapshots = function () {
+          scope.hideSnapshots = !scope.hideSnapshots;
         };
 
-        scope.showNewerSnapDisabled = function(){
+        scope.showNewerSnapDisabled = function () {
           return snapUninitialized() || scope.snapshots.indexOf(scope.curSnap) === 0
         };
-        
-        scope.showOlderSnapDisabled = function(){
+
+        scope.showOlderSnapDisabled = function () {
           return snapUninitialized() || scope.snapshots.indexOf(scope.curSnap) === scope.snapshots.length - 1;
         };
-        
-        scope.showOlderSnap = function(){
+
+        scope.showOlderSnap = function () {
           var idx = scope.snapshots.indexOf(scope.curSnap);
           scope.snapshotSelected(scope.snapshots[idx + 1]);
         };
 
-        scope.showNewerSnap = function(){
+        scope.showNewerSnap = function () {
           var idx = scope.snapshots.indexOf(scope.curSnap);
           scope.snapshotSelected(scope.snapshots[idx - 1]);
         };
 
-        scope.$watch('snapshots', function(){
+        scope.$watch('snapshots', function () {
           // new file selected
           scope.hideSnapshots = false;
         });
 
-        function snapUninitialized(){
+        function snapUninitialized() {
           return typeof scope.curSnap === 'undefined' || typeof scope.snapshots === 'undefined';
         }
       }
