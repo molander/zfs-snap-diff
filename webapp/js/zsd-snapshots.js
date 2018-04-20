e24bf087236bf780fa544a110aedd4be472489a6
unknown <peter.southwell1970@gmail.com>
2018-04-20 15:51:31 -0700

    giving mdDialog confirm a try

--- webapp/js/zsd-snapshots.js
+++ webapp/js/zsd-snapshots.js
@@ -1,5 +1,5 @@
 angular.module('zsdSnapshots', []).
-  directive('zsdSnapshots', ['$location', '$anchorScroll', 'Backend', '$rootScope', function ($location, $anchorScroll, Backend, $rootScope) {
+  directive('zsdSnapshots', ['$location', '$anchorScroll', 'Backend', '$rootScope', '$mdDialog', function ($location, $anchorScroll, Backend, $rootScope, $mdDialog) {
     return {
       restrict: 'E',
       templateUrl: 'template-snapshots.html',
@@ -23,17 +23,27 @@ angular.module('zsdSnapshots', []).
         scope.restoreSnapshot = function ($event, snap) {
           $event.preventDefault();
 
-
-          var r = confirm("Are you sure you'd like to rollback?  Push OK to proceed.");
-          if (r == true) {
-            Backend.restoreSnapshot(scope.curSnap.Name).then(function (res) {
-              $rootScope.$broadcast('zsd:success', res);
-            }).catch((err) => {
-              var string = JSON.stringify(err);
-              alert("Error occured: " + string);
+          $scope.showConfirm = function(ev) {
+            // Appending dialog to document.body to cover sidenav in docs app
+            var confirm = $mdDialog.confirm()
+                  .title('Would you Rollback' + snap.Name + '?')
+                  .textContent('Please confirm you would like to proceed')
+                  .ariaLabel('Snapshot Rollback')
+                  .targetEvent(ev)
+                  .ok('Rollback')
+                  .cancel('Cancel');
+        
+            $mdDialog.show(confirm).then(function() {
+              Backend.restoreSnapshot(scope.curSnap.Name).then(function (res) {
+                $rootScope.$broadcast('zsd:success', res);
+              });
+   
+            }, function() {
             });
-  ;
-          }
+          };
+
+          
+
 
         }
 
